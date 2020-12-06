4b6a134a8bffa200019d56bc8e38e3b4ddf5ff54
jarjanazy <celil.cercenazi@gmail.com>
2020-12-06 15:22:51 +0200

    Handle the case when no base object is given

--- ReduceFunctionImplementation.js
+++ ReduceFunctionImplementation.js
@@ -1,9 +1,21 @@
 function reduce(arr, reduceProcess, baseObject){
-    let result = baseObject;
+    let result = getBaseObject(arr, baseObject);
+    arr = getObjectToIterateOver(arr, baseObject);
+
     arr.forEach(function(e, arr){
         result = reduceProcess(result, e);
     });
     return result;
+}
+
+function getBaseObject(arr, baseObject){
+    if (baseObject === undefined) return arr[0];
+    else return baseObject;
+}
+
+function getObjectToIterateOver(arr, baseObject){
+    if (baseObject === undefined) return arr.slice(1);
+    else return arr;
 }
 
 module.exports = reduce;
--- ReduceFunctionImplementation.test.js
+++ ReduceFunctionImplementation.test.js
@@ -1,7 +1,14 @@
 const reduce = require('./ReduceFunctionImplementation');
 
-test("when reduce is give a list with three numbers, successfully add them", () => {
+test("when reduce is give a list with three numbers and an initial value successfully add them", () => {
     expect(reduce([1,2,10], function(sum, n){
         return sum + n;
     }, 0)).toBe(13);
+});
+
+
+test("when reduce is give a list with three numbers and no inital value, successfully add them", () => {
+    expect(reduce([1,3,10], function(sum, n){
+        return sum + n;
+    })).toBe(14);
 });
